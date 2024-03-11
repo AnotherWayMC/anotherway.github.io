f632bcfaca59bb94ab6414054ed41e92b646d35b
Fundi1330 <kordaaroma@gmail.com>
2024-03-11 18:47:33 +0200

    feat: google ads and hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
 import Main from "./pages/Main.tsx";
