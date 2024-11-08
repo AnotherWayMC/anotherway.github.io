55c1efc8bbdd2c619c6b154938f35ee2d054bcf6
Fundi1330 <kordaaroma@gmail.com>
2024-11-08 14:29:06 +0200

    feat: change map link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,75 +1,70 @@
-import { Link } from "react-router-dom";
-import Logo from "../assets/images/logo.png";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Link } from 'react-router-dom';
+import Logo from '../assets/images/logo.png';
+// import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+// import { ChevronDownIcon } from '@heroicons/react/20/solid';
+// import { useState } from 'react';
 
 function Header() {
+  // const [hamburgerOpened, setHamburgerOpened] = useState(false);
+
+  // const handleHamburgerClick = () => {
+  //   setHamburgerOpened(!hamburgerOpened);
+  // };
+
   return (
-    <header className="sticky top-0 left-0 z-50">
-      <nav className="bg-slate-400 p-3 flex h-full w-screen gap-3">
+    <header className='sticky top-0 left-0 z-50'>
+      <nav className='bg-slate-400 p-3 px-6 flex justify-between h-full w-screen gap-3 relative'>
         <div>
-          <Link to="/">
+          <Link to='/'>
             <img
               src={Logo}
-              alt="Логотип Серверу"
-              className="h-16 object-contain"
+              alt='Логотип Серверу'
+              className='h-16 object-contain'
             />
           </Link>
         </div>
-        <ul className="flex justify-start items-center flex-grow gap-4">
+        <ul className='hidden justify-start items-center gap-4 sm:flex'>
           <li>
-            <Link to="/" className="link">
+            <Link to='/' className='link'>
               Головна
             </Link>
           </li>
           <li>
-            <Menu as="div" className="inline-block text-left">
-              <div className="link">
-                <MenuButton className="flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm text-black">
-                  Мапа
-                  <ChevronDownIcon
-                    aria-hidden="true"
-                    className="-mr-1 h-5 w-5 text-black"
-                  />
-                </MenuButton>
-              </div>
-
-              <MenuItems transition className="relative transition ">
-                <div className="absolute py-1 bg-slate-500 flex flex-col gap-2 w-auto p-4">
-                  <MenuItem>
-                    <Link
-                      to="https://md.map.anotherway.pp.ua/"
-                      className="link"
-                    >
-                      Мапа середньовіччя
-                    </Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <Link
-                      to="https://pl.map.anotherway.pp.ua/"
-                      className="link"
-                    >
-                      Мапа палеоліту
-                    </Link>
-                  </MenuItem>
-                </div>
-              </MenuItems>
-            </Menu>
+            <Link to='https://map.anotherway.pp.ua/' className='link'>
+              Мапа
+            </Link>
           </li>
           <li>
             <Link
-              to="https://anotherway.gitbook.io/anotherway/"
-              className="link"
+              to='https://anotherway.gitbook.io/anotherway/'
+              className='link'
             >
               Вікі
             </Link>
           </li>
           <li>
-            <Link to="/media" className="link">
+            <Link to='/media' className='link'>
               Медіа
             </Link>
           </li>
         </ul>
+        {/* <div className="block sm:hidden text-right">
+          <div
+            onClick={handleHamburgerClick}
+            className="w-12 h-full flex items-center justify-center"
+          >
+            <span
+              className={
+                hamburgerOpened ? "hamburger hamburger-opened" : "hamburger"
+              }
+            ></span>
+          </div>
+        </div>
+        {hamburgerOpened && (
+          <ul className="absolute w-full bg-slate-500 top-0 left-0 p-4 flex flex-col justify-center items-center">
+            <li>Test</li>
+          </ul>
+        )} */}
       </nav>
     </header>
   );
